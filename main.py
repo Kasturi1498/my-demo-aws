89347a1aa89ce1606b357cd562108f19e3e1e953
Kasturi Nandi <kasturi.nandi@claritybiosystems.com>
2022-11-07 19:30:49 +0530

    add upload file option

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import streamlit as st
+import pandas as pd
+st.set_page_config(
+    page_title="My Streamlit App",
+    layout="wide"
+)
 
 st.success("Hello World")
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
